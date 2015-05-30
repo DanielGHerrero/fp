9fd72ab2b6f50ad484bf43ac65e3814a03f3624d
DanielGHerrero <daniel.garcia.herrero@gmail.com>
2015-05-30 11:52:26 +0200

    Correcion del primer ejercicio y borrado para su futura correcion del resto

--- src/main/java/fp/Calculator.java
+++ src/main/java/fp/Calculator.java
@@ -7,21 +7,16 @@ public class Calculator {
 	 * este metodo calcula el seno de un angulo
 	 */
 	static Double sin(double n) {
-		double seno=0;
-		seno=Math.sin(n*2.0*Math.PI/360);
-		return seno;
+		double x = Math.toRadians(n);
+		return (Math.ceil(Math.sin (x)*10))/10 ;
 	}
 
 	/*
 	 * Escribir todos los números del number al 0 de step en step.
 	 */
 	static int[] stepThisNumber(int number, int step) {
-		int[] result = null;
-		for(int cont = 0;cont<number;cont++){
-			number=number-step;
-			result[cont]=number;
-		}
-		return result;
+		
+		return null;
 	}
 
 	/*
@@ -29,12 +24,8 @@ public class Calculator {
 	 * divisores que tiene.
 	 */
 	static int[] divisors(int n) {
-		int rest[] = null;
-		for(int cont=0;cont<n;cont++){
-			if(n%cont==0)
-				rest[cont]=cont;
-		}
-		return rest;
+		
+		return null;
 	}
 	
 	/*
